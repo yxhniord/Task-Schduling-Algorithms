94dc7bff56633a15f639992e7c762ea91a49629a
shahkoma <shah.koma@northeastern.edu>
2023-04-05 10:13:40 -0700

    Updated GWO and CO

--- src/GWO/DatacenterBroker_GWO.java
+++ src/GWO/DatacenterBroker_GWO.java
@@ -11,27 +11,29 @@ import org.cloudbus.cloudsim.core.CloudSim;
 import org.cloudbus.cloudsim.core.CloudSimTags;
 
 public class DatacenterBroker_GWO extends DatacenterBroker {
+	
+	protected int popSize;
+	protected int maxIter;
 
-	protected int iter;
-
-	public DatacenterBroker_GWO(String name, int iter) throws Exception {
+	public DatacenterBroker_GWO(String name, int pop, int iter) throws Exception {
 		super(name);
-
-		this.iter = iter;
+		this.popSize = pop;
+		this.maxIter = iter;
 	}
 
 	@Override
 	protected void submitCloudlets() {
 		List<Cloudlet> clList = getCloudletList();
 		List<Vm> vm_list = getVmsCreatedList();
+		
 		GWOImplement gwo = new GWOImplement();
-		Map<Integer, Integer> allocatedTasks = gwo.allocateTasks(clList, vm_list, iter);
+		Map<Integer, Integer> allocatedTasks = gwo.allocateTasks(clList, vm_list, popSize, maxIter);
 
-		for (int i = 0; i < clList.size(); i++) {
-			Cloudlet cloudlet = clList.get(i);
-			Vm vm = vm_list.get(allocatedTasks.get(i));
-			Log.printLine(CloudSim.clock() + ": " + getName() + ": Sending cloudlet " + cloudlet.getCloudletId()
-					+ " to VM #" + vm.getId());
+		for (Map.Entry<Integer, Integer> entry : allocatedTasks.entrySet()) {
+			Cloudlet cloudlet = clList.get(entry.getKey());
+			Vm vm = vm_list.get(entry.getValue());
+			Log.printLine(CloudSim.clock() + ": " + getName() + ": Sending cloudlet "
+					+ cloudlet.getCloudletId() + " to VM #" + vm.getId());
 			cloudlet.setVmId(vm.getId());
 			sendNow(getVmsToDatacentersMap().get(vm.getId()), CloudSimTags.CLOUDLET_SUBMIT, cloudlet);
 			cloudletsSubmitted++;
